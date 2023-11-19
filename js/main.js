f0b129a17abe083568872cf5ddea832dcb682a8a
yogeshn-1 <nirmalkary@gmail.com>
2023-11-19 10:27:16 +0530

    Adding script to change style of domain-prices when selected and rectifying some styles

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+let domainPricesContainer=document.querySelector(".list.block--domain__prices")
+
+domainPricesContainer.addEventListener("click",function(e){
+    if(e.target.matches('span'))
+    {
+      //To remove styling from the previously selected domain
+      let allListItem= e.target.parentElement.parentElement.children;
+      for (let item of allListItem){
+        item.children[0].removeAttribute("class");
+      }
+      let selectedSpan=e.target;
+      selectedSpan.setAttribute("class","badge badge--secondary");
+    }
+  }
+)
+
